c663e87e1ca77167bc320f8b6a2d62cbe3f0ef8e
Brandon Vo <bvo124@gmail.com>
2017-04-19 18:31:48 -0700

    cleaned up code in server.js

--- server.js
+++ server.js
@@ -64,19 +64,3 @@ app.listen(port, '0.0.0.0', function onStart(error) {
   }
   console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
-
-
-/*
-var express = require('express')
-var app = express()
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})*/
